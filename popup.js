a18ab04fa52bc82208a1caca029d5386923364e9
zhifez <zhifez.lee@gmail.com>
2024-03-03 23:05:36 +0800

    Added link updater to force report and block user

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+const TARGET_BASE_URL = 'https://twitter.com/';
 const TARGET_PAGE_URL = 'https://twitter.com/notifications';
 
 let willReport = true;
@@ -6,6 +7,28 @@ let willBlock = true;
 const checkboxReport = document.getElementById('checkboxReport');
 const checkboxBlock = document.getElementById('checkboxBlock');
 const btnScan = document.getElementById('btnScan');
+const btnForced = document.getElementById('btnForced');
+const btnForcedAuto = document.getElementById('btnForcedAuto');
+
+const ignorePaths = [
+  'home',
+  'notifications',
+];
+
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  // chrome.tabs.update(tabs[0].id, { url });
+  const url = tabs[0].url;
+  const pathname = url.replace(TARGET_BASE_URL, '');
+  if (!url.includes('twitter.com')
+    || ignorePaths.indexOf(pathname) >= 0) {
+    btnForced.setAttribute('disabled', true);
+    btnForcedAuto.setAttribute('disabled', true);
+    return;
+  }
+
+  btnForced.removeAttribute('disabled');
+  btnForcedAuto.removeAttribute('disabled');
+});
 
 checkboxReport.addEventListener('change', function () {
   willReport = !willReport; console.log({ willReport });
@@ -19,10 +42,32 @@ checkboxBlock.addEventListener('change', function () {
 
 btnScan.addEventListener('click', function () {
   const url = `${TARGET_PAGE_URL}?willReport=${willReport}&willBlock=${willBlock}`;
-  // chrome.tabs.create({
-  //   url,
-  // });
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.update(tabs[0].id, { url });
   });
 });
+
+btnForced.addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.update(
+      tabs[0].id, {
+      url: createRNBUrl(tabs[0].url, false),
+    },
+    );
+  });
+});
+
+btnForcedAuto.addEventListener('click', function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.update(
+      tabs[0].id, {
+      url: createRNBUrl(tabs[0].url, true),
+    },
+    );
+  });
+});
+
+function createRNBUrl(url, autoRNB) {
+  const splits = url.split('?autoRNB');
+  return `${splits[0]}?autoRNB=${autoRNB}&willReport=${willReport}&willBlock=${willBlock}`;
+}
